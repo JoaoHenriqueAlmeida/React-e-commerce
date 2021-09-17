1c8484d5334fd5142ad54f000d006e27043d4927
enzo <enzo@pop-os.localdomain>
2021-09-17 16:27:33 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const myResults = await URL.json();
+  return myResults;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const URL = await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const myResults = await URL.json();
+  return myResults;
 }
